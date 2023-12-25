277591f9913543a2b29f5ff7de73c14d94bb3f3a
Mischa Dhar <mischadhar94@gmail.com>
2023-12-25 15:04:16 +0000

    updated code to combine, not add the first and last digit of the calibration

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -2,10 +2,10 @@ def day1():
 
     data = open('input.txt', 'r').read().split('\n')
 
-    data = [[int(y) for y in x if y.isnumeric()] for x in data]
+    data = [[y for y in x if y.isnumeric()] for x in data]
 
     data = [
-        0 if not lst else (lst[0] + lst[-1]) if len(lst) > 1 else lst[0]
+        0 if not lst else (int(lst[0] + lst[-1])) if len(lst) > 1 else int(lst[0])
         for lst in data
     ]
 
