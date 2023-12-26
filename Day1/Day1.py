ebfcc7fbaf774e7e3a4307c0703f7bb14d19cf14
Mischa Dhar <mischadhar94@gmail.com>
2023-12-26 14:51:56 +0000

    updated makefile to include data loader; correct solution to part 2 of the problem

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -1,6 +1,4 @@
-def day1_part1():
-
-    data = open('input.txt', 'r').read().split('\n')
+def day1_part1(data):
 
     data = [[y for y in x if y.isnumeric()] for x in data]
 
@@ -9,8 +7,37 @@ def day1_part1():
         for lst in data
     ]
 
-    return sum(data)
+    return data
+
+def day1_part2(data):
+
+    num_dict = {"one": "o1e"
+                , "two": "t2o"
+                , "three": "t3e"
+                , "four": "f4r"
+                , "five": "f5e"
+                , "six":"s6x"
+                , "seven": "s7n"
+                , "eight": "e8t"
+                , "nine": "n9e"}
+
+    data_clean = []
+
+    for line in data:
+        for key, value in num_dict.items():
+            line = line.replace(key, value)
+        data_clean.append(line)
+
+    calibrations = day1_part1(data_clean)
+
+    return calibrations
 
 if __name__ == "__main__":
 
-    print(day1_part1())
+    data = open('input.txt', 'r').read().split('\n')
+
+    part1 = sum(day1_part1(data))
+
+    part2 = sum(day1_part2(data))
+
+    print(part1, part2)
